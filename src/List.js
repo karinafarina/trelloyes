f9fbf205d842cdc10ba7c2ee628db06a22be96c5
Karina Gaulin <karina@domandtom.com>
2019-07-18 16:30:13 -0400

    Initial commit

---
+++ src/List.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './List.css';
+
+function List(props) {
+  return (
+    <section class="List">
+      <header class="List-header">
+        <h2>{props.header}</h2>
+      </header>
+      <div class="List-cards">
+        <Card title/>
+      </div>
+    </section>
+  )
+}
+
+export default List;
